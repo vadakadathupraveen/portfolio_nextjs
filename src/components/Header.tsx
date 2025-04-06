a0c8e2321317d6bbc34b379b25156add2aa8ec9b
vadakadathupraveen <praveenprakash@pm.me>
2025-04-06 02:02:08 -0400

    Update Header.tsx
    place changed to scarbourough

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => (
     <header className="p-4 bg-blue-600 text-white">
       <h1>Praveen Prakash Vadakadathu</h1>
-      <p>DevOps Engineer | Kitchener, Ontario, Canada</p>
+      <p>DevOps Engineer | Scarborough, Ontario, Canada</p>
     </header>
   );
   
